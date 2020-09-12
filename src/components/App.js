45139f2816d4ac8b99764700224fd5740e83a2bc
nbdehlen <nbdehlen@gmail.com>
2020-09-12 21:04:57 +0200

    Remove import of deleted component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import {P} from '../styles/p';
 import {SectionOne} from '../styles/section';
 import {Container} from '../styles/wrapper';
-import {Counter} from '../features/counter/counter'
 import PostList from './PostList';
 import PostForm from './PostForm';
-function App() {
+const App = () => {
   return (
     <Container>
     
